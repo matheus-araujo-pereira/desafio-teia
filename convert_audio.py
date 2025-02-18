57c892e8ca17a424ed2e7a2a7423410b38c647af
Gabriel Teixeira Silveira <gabriel.silveira.dev@gmail.com>
2025-02-18 09:00:18 -0300

    Adicionei audios ao projeto

--- convert_audio.py
+++ convert_audio.py
@@ -1,11 +1,11 @@
 from pydub import AudioSegment
 
 def convert_m4a_to_wav(input_path: str, output_path: str):
-    audio = AudioSegment.from_file(input_path, format="m4a")
+    audio = AudioSegment.from_file(input_path, format="mp3")
     audio.export(output_path, format="wav")
 
 if __name__ == "__main__":
-    input_path = "/home/matheus/desafio-teia/audio_matheus.m4a"  # Caminho correto do arquivo
-    output_path = "/home/matheus/desafio-teia/audio_matheus.wav"
+    input_path = "roubo-1.mp3"  # Caminho correto do arquivo
+    output_path = "audio-roubo.wav"
     convert_m4a_to_wav(input_path, output_path)
     print(f"Arquivo convertido e salvo em: {output_path}")
